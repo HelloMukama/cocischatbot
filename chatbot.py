cb4b689f6cfd475205ce7bda85abb21d0a3a1f9d
Ntaryebwa Mukama <ntaryebwa.mukama@gmail.com>
2022-09-15 05:36:27 +0300

    update

--- chatbot.py
+++ chatbot.py
@@ -3,22 +3,22 @@ import json  # to be able to load the intents.json file
 import pickle  # for serialization purposes
 import numpy as np  # for bagging process
 
-# import nltk
-# nltk.download('punkt')
-
 import nltk
-import ssl
-
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
-
 nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
+
+# import nltk
+# import ssl
+#
+# try:
+#     _create_unverified_https_context = ssl._create_unverified_context
+# except AttributeError:
+#     pass
+# else:
+#     ssl._create_default_https_context = _create_unverified_https_context
+#
+# nltk.download('punkt')
+# nltk.download('wordnet')
+# nltk.download('omw-1.4')
 
 
 from nltk.stem import WordNetLemmatizer
